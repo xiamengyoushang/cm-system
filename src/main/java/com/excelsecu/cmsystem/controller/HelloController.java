bfba6568e186cdc41d95d7d8a4bc618c09b35789
peng.lei <1q2w3e4r>
2021-08-20 19:07:20 +0800

    新增邮件发送组件

--- src/main/java/com/excelsecu/cmsystem/controller/HelloController.java
+++ src/main/java/com/excelsecu/cmsystem/controller/HelloController.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
